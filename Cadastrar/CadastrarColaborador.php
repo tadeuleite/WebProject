d9397658f2fdc5c42058242b4370b300ef714aef
Tadeu Leite <42976398+tdqc@users.noreply.github.com>
2019-12-03 00:18:12 -0300

    fizanlizacao dos cadastrar

--- Cadastrar/CadastrarColaborador.php
+++ Cadastrar/CadastrarColaborador.php
@@ -9,59 +9,51 @@
 </head>
 
 <body>
-    <div>
-        <h1> Cadastro de Colaboradores </h1>
-        <form name="formularioColaborador" id="formularioColaborador" method="POST">
-            <table>
-                <tr>
-                    <th>
-                        Insira o nome do colaborador
-                    </th>
-                    <th>
-                        <input type="text" name="nomeColaborador" id="nomeColaborador">
-                    </th>
-                </tr>
-                <tr>
-                    <th>
-                        Insira a soft skill do colaborador
-                    </th>
-                    <th>
-                        <input type="text" name="softSkills" id="softSkills">
-                    </th>
-                </tr>
-                <tr>
-                    <th>
-                        Insira a area de concentração desse colaborador
-                    </th>
-                    <th>
-                        <input type="text" name="areaConcentracao" id="areaConcentracao">
-
-                    </th>
-                </tr>
-                <tr>
-                    <th>
-                        Insira o nivel do colaborador nessa area
-                    </th>
-                    <th>
-                        <input type="text" name="nivelGraduacao" id="nivelGraduacao">
-                    </th>
-                </tr>
-                <tr>
-                    <th>
-                        Insira a competência do colaborador
-                    </th>
-                    <th>
-                        <input type="text" name="competencia" id="competencia">
-
-                    </th>
-                </tr>
-            </table>
-        </form>
-    </div>
-    <br>
-    <div>
+    <h1> Cadastro de Colaboradores </h1>
+    <form name="formularioColaborador" id="formularioColaborador" method="POST">
+        <table>
+            <tr>
+                <th>
+                    Insira o nome do colaborador
+                </th>
+                <th>
+                    <input type="text" name="nomeColaborador" id="nomeColaborador">
+                </th>
+            </tr>
+        </table>
         <input type="submit" value="Enviar Informações" id="enviarForm" name="enviarForm">
-    </div>
+    </form>
 </body>
 
 </html>
+
+<?php
+
+$link = mysqli_connect("127.0.0.1", "root", "", "projetoweb");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+}
+
+if (isset($_POST["enviarForm"])) {
+    // Check connection
+    if ($link->connect_error) {
+        die("Connection failed: " . $link->connect_error);
+    }
+
+    if (!empty($_POST["nomeColaborador"])) {
+        $sql = "INSERT INTO colaborador(nome) VALUES ('" . $_POST["nomeColaborador"] . "')";
+
+        if (mysqli_query($link, $sql)) {
+            echo "Cadastrado com sucesso!";
+        } else {
+            echo "Error: " . $sql . "" . mysqli_error($link);
+        }
+    }
+} else {
+    echo ("adasd");
+}
+mysqli_close($link);
+?>
